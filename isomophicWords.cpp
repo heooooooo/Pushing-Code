eda85a3c000168e1050e754ebb4532e1d8256848
Hoieusac <24022789@vnu.edu.vn>
2024-11-04 19:36:14 +0700

    Isomophic words

---
+++ isomophicWords.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isIsomorphic(string a, string b) {
+    if (a.length() != b.length()) return false;
+
+    char mapAtoB[256] = {0};
+    char mapBtoA[256] = {0};
+
+    for (int i = 0; i < a.length(); ++i) {
+        char charA = a[i];
+        char charB = b[i];
+
+        if ((mapAtoB[charA] && mapAtoB[charA] != charB) || (mapBtoA[charB] && mapBtoA[charB] != charA)) {
+            return false;
+        }
+
+        mapAtoB[charA] = charB;
+        mapBtoA[charB] = charA;
+    }
+
+    return true;
+}
