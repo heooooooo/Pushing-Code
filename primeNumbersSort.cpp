ab3d7fb507c2cfa45cf899be3d351918245ae974
Hoieusac <24022789@vnu.edu.vn>
2024-11-09 14:29:22 +0700

    Edit

--- primeNumbersSort.cpp
+++ primeNumbersSort.cpp
@@ -35,7 +35,7 @@ int main(){
     
     for (int i = 0; i < n - 1 ; i++){
         for (int j = i + 1; j < n; j++){
-            if (isPrime(array[i])){
+            if (isPrime(array[i]) && isPrime(array[j])){
                 if (array[i] > array[j]){
                     swap(array[i], array[j]);
                 }
