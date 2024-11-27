a6798dd17e71b8493b3c676bb9576c1de1c28b5b
Hoàng Hiếu <117451221+heooooooo@users.noreply.github.com>
2024-11-27 09:09:41 +0700

    Update hammingDistance.cpp

--- hammingDistance.cpp
+++ hammingDistance.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int hammingDistance(int x, int y) {
     int xorResult = x ^ y;
+    #xorResult = 101 ^ 110 = 011
     int distance = 0;
     while (xorResult) {
         distance += xorResult & 1; // Check the last bit
