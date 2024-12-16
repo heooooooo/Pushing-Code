54c500f2f845b244bd6fba2140ea8f89160f6fc3
Hoàng Hiếu <117451221+heooooooo@users.noreply.github.com>
2024-12-16 09:22:38 +0700

    change

--- pigLatin.cpp
+++ pigLatin.cpp
@@ -1,38 +1,11 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 
 using namespace std;
 
-string pigLatin(string s){
-    string vowels = "ueoai";
-    char firstChar = s[0];
-    bool isVowel = false;
-    
-    for (int i = 0; i < 5; i++){
-        if (firstChar == vowels[i]){
-            isVowel = true;
-            break;
-        }
+string pigLatin(string word) {
+    if (strchr("aeiou", word[0])) {
+        return word + "way";
     }
-    
-    string pigLatinWord = "";
-    if (isVowel){
-        pigLatinWord = s + "way";
-    }
-    else{
-        for (int i = 1; i < s.length(); i++){
-            pigLatinWord += s[i];
-        }
-        pigLatinWord += firstChar;
-        pigLatinWord += "ay";
-    }
-    return pigLatinWord;
-}
-
-int main(){
-    string s;
-    cin >> s;
-    
-    cout << pigLatin(s);
-
+    return word.substr(1) + word[0] + "ay";
 }
