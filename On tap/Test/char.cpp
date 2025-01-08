03061fda9e63c2b59c4abcf3d24711860fc03122
Hoàng Hiếu <117451221+heooooooo@users.noreply.github.com>
2025-01-08 22:09:55 +0700

    Update char.cpp

--- On tap/Test/char.cpp
+++ On tap/Test/char.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main(){
     string s; getline(cin, s);
-    int count[26] = {0};
+    int count[26] = {0}; // Tạo mảng độ lớn 26 tương ứng 26 ký tự thường
 
     for (char ch : s){
-        count[ch - 'a']++;
+        count[ch - 'a']++; // Duyệt từng ký tự trong s và tăng vị trí tương ứng của kí tự đó lên 1 đơn vị
     }
-
+    // Ví dụ kí tự ch = b = 98 thì ch - 'a' tương đương với 98 - 97 = 1 vị trí index = 1
     for (int i = 0; i < 26; i++) {
         if (count[i]) {
             char out = 'a' + i;
@@ -18,3 +18,5 @@ int main(){
         }
     }
 }
+
+
