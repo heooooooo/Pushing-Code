0029dacf0b10c69d5a8cc03c6deb4e055e033bf6
Hoàng Hiếu <117451221+heooooooo@users.noreply.github.com>
2025-01-08 22:13:16 +0700

    Update timeStruct.cpp

--- On tap/Test/timeStruct.cpp
+++ On tap/Test/timeStruct.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 struct Time{
+//Khởi tạo cấu trúc Time gồm 2 thành phần là hour và minute
     int hour, minute;
     Time (int initHour, int initMinute){
         hour = initHour;
@@ -12,18 +13,20 @@ struct Time{
 
 Time tong(const Time& tm1, const Time& tm2){
     int carry = 0;
-    int minSum = tm1.minute + tm2.minute;
-    minSum %= 60;
-    carry = 1;
+    int minSum = tm1.minute + tm2.minute; // Cộng phút của 2 cấu trúc
+    if (minSum > 60){ //Nếu tổng số phút lớn hơn 60 thì đặt carry thành 1 và tổng phút lấy phần dư ngoài 60
+        minSum %= 60;
+        carry = 1;
+    }
     int hourSum = tm1.hour + tm2.hour + carry;
 
-    return Time(hourSum, minSum);
+    return Time(hourSum, minSum); // Trả về cấu trúc time tương ứng
 }
 
 bool ssLonHon(const Time& tm1, const Time& tm2){
-    int totalMin1 = tm1.hour * 60 + tm1.minute;
+    int totalMin1 = tm1.hour * 60 + tm1.minute; 
     int totalMin2 = tm2.hour * 60 + tm2.minute;
-
+    // So sánh tổng số phút của 2 thời giam
     return totalMin1 > totalMin2;
 }
 
